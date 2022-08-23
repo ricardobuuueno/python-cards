96a66de6d65558d2e93cb9d4859690ff057778e4
Ricardo Bueno <ricardo.bueno.sf.dev@hotmail.com>
2022-08-23 10:25:46 -0300

    Last commit of the project

--- poker/hand.py
+++ poker/hand.py
@@ -13,6 +13,20 @@ from poker.validators import (
 )
 
 class Hand():
+    VALIDATORS = (
+        RoyalFlushValidator,
+        StraightFlushValidator,
+        FourOfAKingValidator,
+        FullHouseValidator,
+        FlushValidator,
+        StraightValidator,
+        ThreeOfAKindValidator,
+        TwoPairValidator,
+        PairValidator,
+        HighCardValidator,
+        NoCardsValidator
+    )
+
     def __init__(self):
         self.cards = []
 
@@ -26,24 +40,8 @@ class Hand():
         copy.sort()
         self.cards = copy
 
-    @property
-    def _rank_validations_from_best_to_worst(self):
-        return  (
-            ("Royal Flush", RoyalFlushValidator(cards = self.cards).is_valid),
-            ("Straight Flush", StraightFlushValidator(cards = self.cards).is_valid),
-            ("Four of a Kind", FourOfAKingValidator(cards = self.cards).is_valid),
-            ("Full House", FullHouseValidator(cards = self.cards).is_valid),
-            ("Flush", FlushValidator(cards = self.cards).is_valid),
-            ("Straight", StraightValidator(cards = self.cards).is_valid),
-            ("Three of a Kind", ThreeOfAKindValidator(cards = self.cards).is_valid),
-            ("Two Pair", TwoPairValidator(cards = self.cards).is_valid),
-            ("Pair", PairValidator(cards = self.cards).is_valid),
-            ("High Card", HighCardValidator(cards = self.cards).is_valid),
-            ("No Cards", NoCardsValidator(cards = self.cards).is_valid)
-        )
-
     def best_rank(self):
-        for rank in self._rank_validations_from_best_to_worst:
-            name, validator_func = rank
-            if validator_func():
-                return name
+        for index, validator_klass in enumerate(self.VALIDATORS):
+            validator = validator_klass(cards = self.cards)
+            if validator.is_valid():
+                return (index, validator.name, validator.valid_cards())
