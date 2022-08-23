236f9982716816d8643a0a3305c344aa0182a376
Ricardo Bueno <ricardo.bueno.sf.dev@hotmail.com>
2022-08-23 08:53:56 -0300

    Finalizing validators

--- poker/hand.py
+++ poker/hand.py
@@ -1,4 +1,10 @@
 from poker.validators import (
+    RoyalFlushValidator,
+    StraightFlushValidator,
+    FourOfAKingValidator,
+    FullHouseValidator,
+    FlushValidator,
+    StraightValidator,
     ThreeOfAKindValidator,
     TwoPairValidator,
     PairValidator,
@@ -23,12 +29,12 @@ class Hand():
     @property
     def _rank_validations_from_best_to_worst(self):
         return  (
-            ("Royal Flush", self._royal_flush),
-            ("Straight Flush", self._straight_flush),
-            ("Four of a Kind", self._four_of_a_kind),
-            ("Full House", self._full_house),
-            ("Flush", self._flush),
-            ("Straight", self._straight),
+            ("Royal Flush", RoyalFlushValidator(cards = self.cards).is_valid),
+            ("Straight Flush", StraightFlushValidator(cards = self.cards).is_valid),
+            ("Four of a Kind", FourOfAKingValidator(cards = self.cards).is_valid),
+            ("Full House", FullHouseValidator(cards = self.cards).is_valid),
+            ("Flush", FlushValidator(cards = self.cards).is_valid),
+            ("Straight", StraightValidator(cards = self.cards).is_valid),
             ("Three of a Kind", ThreeOfAKindValidator(cards = self.cards).is_valid),
             ("Two Pair", TwoPairValidator(cards = self.cards).is_valid),
             ("Pair", PairValidator(cards = self.cards).is_valid),
@@ -41,56 +47,3 @@ class Hand():
             name, validator_func = rank
             if validator_func():
                 return name
-
-    def _royal_flush(self):
-        is_straight_flush = self._straight_flush()
-        if not is_straight_flush:
-            return False
-
-        is_royal = self.cards[-1].rank == "Ace"
-        return is_straight_flush and is_royal
-
-    def _straight_flush(self):
-        return self._flush() and self._straight()
-
-    def _four_of_a_kind(self):
-        ranks_with_four_of_a_kind = self._ranks_with_count(4)
-        return len(ranks_with_four_of_a_kind) == 1
-
-    def _full_house(self):
-        return ThreeOfAKindValidator(cards = self.cards).is_valid() and PairValidator(cards = self.cards).is_valid()
-
-    def _flush(self):
-        suit_that_occurs_5_or_more_times = {
-            suit: suit_count
-            for suit, suit_count in self._card_suit_counts.items()
-            if suit_count >= 5
-        }
-        return len(suit_that_occurs_5_or_more_times) == 1
-
-    def _straight(self):
-        rank_idxs = [card.rank_index for card in self.cards]
-        return len(rank_idxs) == 5 and rank_idxs == list(range(rank_idxs[0], rank_idxs[-1] + 1))
-
-    def _ranks_with_count(self, count):
-        return {
-            rank: rank_count
-            for rank, rank_count in self._card_rank_counts.items()
-            if rank_count == count
-        }
-
-    @property
-    def _card_suit_counts(self):
-        card_suit_counts = {}
-        for card in self.cards:
-            card_suit_counts.setdefault(card.suit, 0)
-            card_suit_counts[card.suit] += 1
-        return card_suit_counts
-
-    @property
-    def _card_rank_counts(self):
-        card_rank_counts = {}
-        for card in self.cards:
-            card_rank_counts.setdefault(card.rank, 0)
-            card_rank_counts[card.rank] += 1
-        return card_rank_counts
